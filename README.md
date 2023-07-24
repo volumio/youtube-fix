4af83bf0de96f26736fd942e0c6181b5c156d48c
Volumio <info@volumio.org>
2023-07-24 13:44:13 +0200

    YT fix

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-"# youtube-fix" 
+### YOUTUBE FIX 
+
+This is an utility to unbrick volumio devices with the youtube plugin 
+
+IMPORTANT: 
+- Your device must be off when this procedure starts 
+- Your device must be turned on when requested (point 5)
+- Make sure your device is connected to the same network as your PC 
+
+1) Download Nodejs 
+
+Windows 
+https://nodejs.org/dist/v18.17.0/node-v18.17.0-x64.msi
+
+
+MAC 
+https://nodejs.org/dist/v18.17.0/node-v18.17.0.pkg
+
+2) Install Nodejs 
+
+3) Download and unpack this utility 
+
+https://github.com/volumio/youtube-fix/archive/refs/heads/master.zip
+
+Unpack this utility 
+
+4) Open in a terminal with 
+
+node index.js 
+
+5) Turn on the affected device 
+
+6) After 2 minutes from starting up your device, please restart it (remove and reattach power)
+
--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 var io = require('socket.io-client');
 
-var socket = io.connect('http://192.168.1.95:3000');
+
+var mdns = require('mdns-js');
+mdns.excludeInterface('0.0.0.0');
+var browser = mdns.createBrowser();
 
 
-socket.on('connect', function() {
-	console.log('Device found');
-	console.log('Uninstalling Youtube Plugins');
-	socket.emit('unInstallPlugin', {'category':'music_service','name':'youtube2'});
-	socket.emit('unInstallPlugin', {'category':'music_service','name':'ytmusic'});
+var alreadyShownIPArray = [];
+var intervalDetection = 500;
+
+browser.on('ready', function () {
+  browser.discover();
+  setInterval(()=>{
+    browser.discover();
+  }, intervalDetection)
 });
+
+browser.on('update', function (data) {
+    parseOutputData(data);
+});
+
+
+function parseOutputData(data) {
+  for (var i in data.type) {
+    if (data.type[i].name.includes('Volumio')) {
+      connectAndUninstall(data);
+    }
+  }
+}
+
+
+function connectAndUninstall(data) {
+	var ip = data.addresses[0];
+	var deviceUrl = 'http://' + ip + ':3000';
+	var socket = io.connect(deviceUrl);
+	
+	console.log('Connecting to ' + deviceUrl);
+
+
+	socket.on('connect', function() {
+		console.log('Device found');
+		console.log('Uninstalling Youtube Plugins');
+		socket.emit('unInstallPlugin', {'category':'music_service','name':'youtube2'});
+		socket.emit('unInstallPlugin', {'category':'music_service','name':'ytmusic'});
+	setTimeout(()=>{
+		console.log('DONE, Please restart your device');
+		}, 10000);
+	});
+}
+
+
+
+
